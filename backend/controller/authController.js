3540e0994e0da2b0b88e9311689f2e88a4f99694
Kaiser Fayaz <kasirmalik87@gmail.com>
2025-07-19 08:50:40 +0530

    added nodemailer for email and otp verification

--- backend/controller/authController.js
+++ backend/controller/authController.js
@@ -1,6 +1,7 @@
 import userModel from "../models/userModel.js";
 import jwt from "jsonwebtoken";
 import bcrypt from "bcryptjs";
+import transporter from "../config/nodemailer.js";
 
 export const register = async (req, res) => {
   const { name, email, password } = req.body;
@@ -30,6 +31,17 @@ export const register = async (req, res) => {
       sameSite: process.env.NODE_ENV === "production" ? "none" : "strict",
       maxAge: 1000 * 60 * 60 * 24 * 7, // 7 days
     });
+
+     /// to send email
+     const mailOptions = {
+       from: process.env.SENDER_EMAIL,
+       to: email,
+       subject: "Welcome to Auth Project",
+       text: `Welcome to Auth Project, ${email}!`,
+     };
+     await transporter.sendMail(mailOptions)
+
+
     res.json({ sucess: true, message: "User registered successfully" });
   } catch (error) {
     res.json({ sucess: false, message: "Error in register" });
@@ -81,3 +93,46 @@ export const logout = async (req, res) => {
     res.json({ sucess: false, message: "Error in logout" });
   }
 };
+
+// to verify otp
+
+export const sendVerifyOtp = async (req, res) =>{
+  try {
+    const {userId} = req.body;
+    const user = await userModel.findById(userId);
+    if (!user){
+      res.json({ sucess: false, message: "User not found" });
+    }
+    
+    if(user.isAccountVerified){
+      res.json({ sucess: false, message: "User already verified" });
+    }
+
+    // gen otp 
+    const otp =String(Math.floor(100000 + Math.random() * 900000))
+
+    user.verifyOtp = otp;
+    user.verifyOtpExpiryAt = Date.now() + 1000 * 60 * 20;
+    await user.save();
+    // send otp to user email
+
+    const mailOption = {
+      from: process.env.SENDER_EMAIL,
+      to: user.email,
+      subject: "Account verification OTP",
+      text: `Your Otp is, ${otp}.verify this 20 minutes!`,
+    };
+    await transporter.sendMail(mailOption);
+
+    res.json({ sucess: true, message: "OTP sent successfully" });
+    
+    
+
+  } catch (error) {
+    res.json({ sucess: false, message: "Error in verify otp" });
+  }
+};
+
+// 
+
+
