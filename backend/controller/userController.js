1c0f0834d9628753487f82ff9f5eedd9528dcd1d
Kaiser Fayaz <kasirmalik87@gmail.com>
2025-07-20 13:39:16 +0530

    added

---
+++ backend/controller/userController.js
@@ -0,0 +1,23 @@
+import userModel from "../models/userModel.js";
+
+
+export const getUserData = async (req, res) => {
+    try {
+      const {userId} = req.body;
+      const user = await userModel.findById(userId);
+      if (!user) {
+        return res.status(404).json({ message: "User not found" });
+      };
+      res.json({
+        sucess:true,
+        userdata:{
+            name :user.name,
+            isAccountVerified :user.isAccountVerified,
+        }
+
+      });
+
+    } catch (error) {
+        res.json({ sucess: false, message: error.message });
+    }
+}
--- backend/server.js
+++ backend/server.js
@@ -4,6 +4,7 @@ import "dotenv/config";
 import cookieParser from "cookie-parser";
 import connectDb from "./config/mongodb.js";
 import authRouter from "./routes/authRoute.js";
+import userRouter from "./routes/userRoutes.js";
 
 const app = express();
 
@@ -28,6 +29,7 @@ app.get("/", (req, res) => {
   res.send("Api Working Fine");
 });
 app.use("/api/auth", authRouter);
+app.use("/api/user", userRouter);
 
 app.listen(port, () => {
   console.log(`Server is running on port ${port}`);
