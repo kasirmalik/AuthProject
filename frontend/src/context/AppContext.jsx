8a4e66bbb31d2e59ee63c3e0fea2de174c98d520
Kaiser Fayaz <kasirmalik87@gmail.com>
2025-07-22 10:13:18 +0530

    added all files

---
+++ frontend/src/context/AppContext.jsx
@@ -0,0 +1,32 @@
+import axios from "axios";
+import { createContext, useState } from "react";
+import { toast } from "react-toastify";
+
+export const AppContext = createContext();
+
+export const AppContextProvider = (props) => {
+  const backendUrl = import.meta.env.VITE_BACKEND_URL;
+
+  const [isLoggedIn, setIsLoggedIn] = useState(false);
+  const [userData, setUserData] = useState(false);
+
+  const getUserData = async () => {
+    try {
+      const { data } = await axios.get(backendUrl + "/api/user/data");
+      data.sucess ? setUserData(data.user) : toast.error(data.message)
+    } catch (error) {
+      toast.error(error.message);
+    }
+  }
+
+  const value = {
+    backendUrl,
+    isLoggedIn,
+    setIsLoggedIn,
+    setUserData,
+    userData,
+    getUserData,
+  };
+
+  return <AppContext.Provider value={value}>{props.children}</AppContext.Provider>;
+};
